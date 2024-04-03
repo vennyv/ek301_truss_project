85e8067b9f3a57c812f6c9556920a179106f3d05
Darr320 <165436969+Darr320@users.noreply.github.com>
2024-04-03 16:05:32 -0400

    Add files via upload

--- EK301TrussProjectTest.m
+++ EK301TrussProjectTest.m
@@ -1,11 +1,12 @@
-% Define C matrix
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Input C matrix
 C=[1 1 0 0 0 0 0 0 0 0 0 0 0;1 0 1 1 0 0 0 0 0 0 0 0 0;0 0 0 1 1 1 0 0 0 0 0 0 0;0 1 1 0 1 0 1 1 0 0 0 0 0;0 0 0 0 0 1 1 0 1 1 0 0 0;0 0 0 0 0 0 0 1 1 0 1 1 0;0 0 0 0 0 0 0 0 0 1 1 0 1; 0 0 0 0 0 0 0 0 0 0 0 1 1];
 
 sizes=size(C);
 joints=sizes(1,1);
 members=sizes(1,2);
 
-%Define Sx and Sy matrices
+%Input Sx and Sy matrices
 Sx=zeros(8,3);
 Sx(1,1)=1;
 
@@ -13,13 +14,15 @@ Sy=zeros(8,3);
 Sy(1,2)=1;
 Sy(8,3)=1;
 
-%Define X and Y vectors
+%Input X and Y vectors
 X=[0;0;4;4;8;8;12;12];
 Y=[0;4;8;4;8;4;4;0];
 
-%Define L vector
+%Input L vector
 L=zeros(16,1);
 L(12)=25;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %Setting up A vector
 A=zeros(joints*2,members+3);
@@ -28,27 +31,28 @@ A(joints+1:joints*2,1:members)=C;
 
 [a b]=find(C==1);
 
-Locations=[a b];
-
 
 %Calculate total distance
-%tdist=zeros(13,1);
-%for x = 1:2:members*2
-   % xdist=(X(a(x+1))-X(a(x)));
-   % xdistsq=xdist^2;
-   % ydist=(Y(a(x+1))-Y(a(x)));
-   % ydistsq=ydist^2;
-   % if x==1
-   %     tdist(x)=sqrt(xdistsq+ydistsq);
-  %  else 
-   %     tdist(x-1)=sqrt(xdistsq+ydistsq);
-%end
-%end
+distvec=zeros(13,1);
+for x = 1:2:members*2
+    xdist=(X(a(x+1))-X(a(x)));
+    xdistsq=xdist^2;
+    ydist=(Y(a(x+1))-Y(a(x)));
+    ydistsq=ydist^2;
+    if x==1
+       distvec(x)=sqrt(xdistsq+ydistsq);
+    else 
+        distvec(x-1)=sqrt(xdistsq+ydistsq);
+end
+end
+
+distvec=nonzeros(distvec');
+tdist=sum(distvec);
 
 %Calculate total cost
-%cost=10*joints+tdist;
+cost=10*joints+tdist;
 
-%Calculating A vectorx
+%Calculating A vector
 for x = 1:2:members*2
     xdist=(X(a(x+1))-X(a(x)));
     xdistsq=xdist^2;
@@ -70,10 +74,12 @@ for x = 1:2:members*2
 end
 
 A(1:joints,members+1:members+3)=Sx;
-A(joints+1:joints*2,members+1:members+3)=Sy
+A(joints+1:joints*2,members+1:members+3)=Sy;
 
 %Calculate T vector
 T=A^-1 * L;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %Print Results
 fprintf('EK301, Section A2, Group 20: Darren S., Venessa M., Vikram B. 4/2/2024.\n');
@@ -94,5 +100,5 @@ fprintf('Sx1: %.2f\n',T(members+1));
 fprintf('Sy1: %.2f\n',T(members+2));
 fprintf('Sy2: %.2f\n',T(members+3));
 
-%fprintf('Cost of truss: $%.1f\n',cost)
+fprintf('Cost of truss: $%.2f\n',cost)
 
